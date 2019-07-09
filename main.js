9fcdff57f7d52797f8e13642c098e6c629e99d0a
CielleXIV <52672791+CielleXIV@users.noreply.github.com>
2019-07-09 19:11:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     $("#test").html("Score: " + score + "<br>Average: 0<br>Start whenever you're ready.");
     $("counting").html("test");
 
-        $("#test").html("Score: " + score + "<br>Average: " + average + " cps.<br>You have 60 seconds to click");
     $("#button").mousedown(function () {
         score = score + 1;
-        average = score / 60;
+        average = score / 5;
+        $("#test").html("Score: " + score + "<br>Average: " + average + " cps.<br>You have 5 seconds to click");
         startTimer();
     });
 
@@ -17,16 +17,16 @@ $(document).ready(function () {
             var counter = 0;
             var interval = setInterval(function () {
                 counter++;
-                display = 60 - counter;
+                display = 5 - counter;
                 $("#button").html("CLICK! (" + display + " secs)");
-                if (counter == 60) {
-                    alert("It's been 60 seconds, stop clicking now.");
+                if (counter == 5) {
+                    alert("It's been 5 seconds, stop clicking now.");
                     clearInterval(interval);
                     stop = 0;
                     endscore = score;
                     score = 0;
                     $("#results").html("You clicked " + endscore + " times, in 5 seconds.<br>Your average click speed was " + average + " clicks per second.<br>Start clicking again to retry and get a better score!");
-                    $("#button").html("CLICK! (60 secs)");
+                    $("#button").html("CLICK! (5 secs)");
                 }
             }, 1000);
         }
